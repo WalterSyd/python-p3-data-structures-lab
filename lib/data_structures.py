353415f5e07a26e700979c3fbc5137acba750d9e
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-09-01 19:33:01 +0300

    Added get spicy food by cuisine function

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -31,6 +31,7 @@ def print_spicy_foods(spicy_foods):
     for food in spicy_foods:
         print(f"{food['name']} ({food['cuisine']}) | Heat Level: {'🌶' * food['heat_level']}")
 
+
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
     for food in spicy_foods:
         if food["cuisine"] == cuisine:
